34021c4552f71406af94ad5bcc35c4ac84364d6d
chapcs <benchapman003@gmail.com>
2023-07-29 09:27:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿abstract class MyAbstractClass { }
+
+class Program
+{
+    MyAbstractClass myInstance = new MyAbstractClass();
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
