25762a7785f8d194f07c35b08b0ec10fcea60695
chapcs <benchapman003@gmail.com>
2023-07-29 09:28:00 -0400

    notes

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    MyAbstractClass myInstance = new MyAbstractClass();
+    MyAbstractClass myInstance = new MyAbstractClass();//intentional error to show it cannot be instantiated
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
